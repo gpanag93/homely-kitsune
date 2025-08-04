9c59ba5ae09572d710fb77d890221f0da79b47a6
gpanag93 <gpanag93@gmail.com>
2025-08-04 14:04:08 +0200

    Created fake endpoints to avoid bot detection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -9,4 +10,20 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('favicon.ico')
+  getFavicon(@Res() res: Response) {
+    return res.status(204).send(); // No Content
+  }
+
+  @Get('robots.txt')
+  getRobots(@Res() res: Response) {
+    return res.type('text/plain').send('User-agent: *\nDisallow: /');
+  }
+
+  @Get('.well-known/security.txt')
+  getSecurityTxt(@Res() res: Response) {
+    return res.type('text/plain').send('');
+  }
+
 }
